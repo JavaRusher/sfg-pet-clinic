cdaeeea2244554f3255905e830a58723192b5190
Iosif Yusufov <iosif88@gmail.com>
2018-10-16 21:42:08 +0400

    Implement Map based services

---
+++ pet-clinic-data/src/main/java/edu/javarush/sfgpetclinic/service/map/PetServiceMap.java
@@ -0,0 +1,37 @@
+package edu.javarush.sfgpetclinic.service.map;
+
+import edu.javarush.sfgpetclinic.model.Pet;
+import edu.javarush.sfgpetclinic.service.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long>  implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+
+
+
+}
