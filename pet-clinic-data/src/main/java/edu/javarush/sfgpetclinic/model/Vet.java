c852b26b93faa43f8c262766cc75c1a34e8cdb8b
Iosif Yusufov <iosif88@gmail.com>
2018-10-21 13:41:36 +0400

    added Speciality

--- pet-clinic-data/src/main/java/edu/javarush/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/edu/javarush/sfgpetclinic/model/Vet.java
@@ -1,5 +1,18 @@
 package edu.javarush.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Speciality> specialites;
+    public Set<Speciality> getSpecialites() {
+        return specialites;
+    }
+
+    public void setSpecialites(Set<Speciality> specialites) {
+        this.specialites = specialites;
+    }
+
+
 
 }
