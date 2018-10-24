a2ca95b70020f7591052c307a4cb6c39f59ebfee
Iosif Yusufov <iosif88@gmail.com>
2018-10-24 12:20:33 +0400

    Created JPA Entity

--- pet-clinic-data/src/main/java/edu/javarush/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/edu/javarush/sfgpetclinic/model/PetType.java
@@ -1,9 +1,16 @@
 package edu.javarush.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity{
 
-
+    @Column(name = "name")
     private String name;
+
 
     public String getName() {
         return name;
