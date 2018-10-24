43b96e44d7cd5db501118fbb63f34fdb3bc03df2
Iosif Yusufov <iosif88@gmail.com>
2018-10-24 15:18:27 +0400

    profiles for JPA and map services

--- pet-clinic-data/src/main/java/edu/javarush/sfgpetclinic/service/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/edu/javarush/sfgpetclinic/service/map/OwnerMapService.java
@@ -5,11 +5,13 @@ import edu.javarush.sfgpetclinic.model.Pet;
 import edu.javarush.sfgpetclinic.service.OwnerService;
 import edu.javarush.sfgpetclinic.service.PetService;
 import edu.javarush.sfgpetclinic.service.PetTypeService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default","map"})
 public class OwnerMapService extends AbstractMapService<Owner, Long>
         implements OwnerService {
 
--- pet-clinic-data/src/main/java/edu/javarush/sfgpetclinic/service/map/VetMapService.java
+++ pet-clinic-data/src/main/java/edu/javarush/sfgpetclinic/service/map/VetMapService.java
@@ -4,11 +4,13 @@ import edu.javarush.sfgpetclinic.model.Speciality;
 import edu.javarush.sfgpetclinic.model.Vet;
 import edu.javarush.sfgpetclinic.service.SpecialityService;
 import edu.javarush.sfgpetclinic.service.VetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default","map"})
 public class VetMapService extends AbstractMapService<Vet,Long> implements VetService {
 
     private final SpecialityService specialtyService;
